6c8fde9db42db31a457da27eb8893a10956d9fd1
caishijian2008 <442754043@qq.com>
2016-12-06 00:44:55 +0800

    添加了登录和注册的tab

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+jQuery(document).ready(function($) {
+  
+  /* scroll to top JS */
+  $(".fixed-btn").hide();
+  $(function() {
+	  $(window).scroll(function(e) {
+	    if($(window).scrollTop() >= 100) {
+	      $(".fixed-btn").fadeIn();
+	    } else {
+	      $(".fixed-btn").fadeOut();
+	    }
+	  });
+  });
+  $(".fixed-btn").click(function(e) {
+    $("html, body").animate({scrollTop: 0}, 300); 
+    return false; 
+  }); 
 
+  
+
+});
