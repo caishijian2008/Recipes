aabdbf68156a85ffe34216cacbbab48911212eca
caishijian2008 <442754043@qq.com>
2017-01-03 17:51:44 +0800

    添加首页推荐部分动画

--- js/main.js
+++ js/main.js
@@ -1,5 +1,35 @@
-jQuery(document).ready(function($) {
+$(document).ready(function() {
   
+  /* 首页循环播放的动画 */
+  function ztlistMoveleft() {
+    $("#ztlist_item_list_li").animate({"marginleft": "0px"}, 500, function() {
+      $("#ztlist_item_list_li").last().prependTo($("#ztlist_item_list_ul"));
+      $("#ztlist_item_list_li").css('margin-left', '-198px');
+      
+    });
+  }
+
+  function ztlistMoveright() {
+    $("#ztlist_item_list_li").animate({"marginleft": "-396px"}, 500, function() {
+      $("#ztlist_item_list_li").first().appendTo($("#ztlist_item_list_ul"));
+      $("#ztlist_item_list_li").css('margin-left', '0');
+    });
+  }
+
+  var index_cd_move = "";
+
+  $(function() {
+    if ($("#ztlist_item_list_li").length > 0) {
+      index_cd_move = setInterval(function(){ztlistMoveright();},5000);
+      $("#ztlist_item_list_ul").mouseenter(function() {
+        clearInterval(index_cd_move);
+      });
+      $("#ztlist_item_list_ul").mouseleave(function() {
+        index_cd_move = setInterval(function(){ztlistMoveright();},5000);
+      });
+    }
+  });
+
   /* scroll to top JS */
   $(".fixed-btn").hide();
   $(function() {
@@ -15,7 +45,5 @@ jQuery(document).ready(function($) {
     $("html, body").animate({scrollTop: 0}, 300); 
     return false; 
   }); 
-
-  
 
 });
