f66315bd523988b9769bc19adf0832f1475caf25
caishijian2008 <442754043@qq.com>
2017-01-10 23:40:57 +0800

    其他修改

--- js/main.js
+++ js/main.js
@@ -1,5 +1,57 @@
-$(document).ready(function() {
-  
+/*$(document).ready(function() {*/
+(function($) {
+  /* 注册用户 */
+  $(function() {
+    /* 自定义的验证方法 */
+    jQuery.validator.addMethod("regexp",function(value, element) {
+      return this.optional(element) || /^[A-Za-z0-9]+$/.test(value);
+    },"Just only English and digits!");
+
+    $('.signupform').validate({
+      submitHandler: function(form) {
+        alert("注册提交事件！");
+        $(form).ajaxSubmit();
+      },
+      rules: {
+        username: {
+          required: true,
+          rangelength: [6,20],
+          regexp: true
+        },
+        email: "required",
+        password: {
+          required: true,
+          minlength: 6
+        },
+        confirm_password: {
+          required: true,
+          minlength: 6,
+          equalTo: "#password"
+        },
+        agree: "required"
+      },
+      messages: {
+        username: {
+          required: "请输入用户名！",
+          rangelength: "只能在6-20个字符之间",
+          regexp: "只能是英文和数字的组合"
+        },
+        email: "请输入一个正确的邮箱！",
+        password: {
+          required: "请输入密码！",
+          minlength: "密码长度不能小于6位！"
+        },
+        confirm_password: {
+          required: "再次输入密码！",
+          minlength: "密码长度不能小于6位！",
+          equalTo: "两次密码输入不一致"
+        },
+        agree: "请同意我们的协议！"
+      }
+    });
+    
+  });
+
   /* 首页循环播放的动画 */
   /*function ztlistMoveleft() { 
     $("#ztlist_item_list_ul").animate({"marginLeft": "0"}, 500, function() {
@@ -54,4 +106,4 @@ $(document).ready(function() {
     return false; 
   }); 
 
-});
+})(jQuery);
